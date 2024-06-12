62c7d91e137a69ec4323057331211223a425a93e
wazirul azzan <mwazirulazzan@gmail.com>
2024-06-12 14:49:39 +0700

    feat : message menu

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,20 +1,52 @@
 import { Imessage } from '@/lib/store/messages'
 import React from 'react'
 import Image from 'next/image'
+import { Ellipsis } from 'lucide-react';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { useUser } from '@/lib/store/user';
+
 
 export default function Message({ message }: { message: Imessage }) {
+  const user = useUser((state) => state.user);
+
   return (
     <div className='flex gap-2'>
       <div>
         <Image src={message.users?.avatar_url!} alt={message.users?.display_name!} width={40} height={40} className='rounded-full ring-2'/>
       </div>
       <div className='flex-1'>
-        <div className='flex items-center gap-1'>
-          <h1 className='font-bold'>{message.users?.display_name!}</h1>
-          <h1 className='text-sm text-gray-400'>{new Date(message.created_at).toDateString()}</h1>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center gap-1'>
+            <h1 className='font-bold'>{message.users?.display_name!}</h1>
+            <h1 className='text-sm text-gray-400'>{new Date(message.created_at).toDateString()}</h1>
+          </div>
+          {  message.users?.id === user?.id && <MessageMenu /> }
         </div>
         <p className='text-gray-300'>{message.text}</p>
       </div>
     </div>
+  )
+}
+
+const MessageMenu = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Ellipsis />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>Action</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Edit</DropdownMenuItem>
+        <DropdownMenuItem>Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
