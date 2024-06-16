35162dc86fcf75cd1bfb509779bde1b24687fa5d
wazirul azzan <mwazirulazzan@gmail.com>
2024-06-16 21:28:30 +0700

    feat : listen realtime

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -3,36 +3,35 @@ import React from 'react'
 import { Input } from "@/components/ui/input"
 import { supabaseBrowser } from '@/lib/supabase/browser'
 import { toast } from 'sonner'
-import { v4 as uuid } from 'uuid'
+import { v4 as uuidv4 } from 'uuid'
 import { useUser } from '@/lib/store/user'
-import { useMessage } from '@/lib/store/messages'
-import { Imessage } from '@/lib/store/messages'
+import { useMessage, Imessage } from '@/lib/store/messages'
 
 export default function ChatInput() {
   const supabase = supabaseBrowser();
   const user = useUser((state) => state.user);
   const addMessage = useMessage((state) => state.addMessage);
-
+  const setOptimisticIds = useMessage((state) => state.setOptimisticIds);
   const handleSendMessage = async (text:string) => {
     if(text.trim()){
+			const id = uuidv4();
       const newMessage = {
-        id: uuid(),
+        id,
         text,
         send_by: user?.id,
         is_edit:false,
         created_at: new Date().toISOString(),
         users:{
+          id: user?.id,
           avatar_url: user?.user_metadata?.avatar_url,
           created_at: new Date().toISOString(),
           display_name: user?.user_metadata?.user_name,
-          id: user?.id
-        }
+        },
       };
   
       addMessage(newMessage as Imessage);
-      // alert(text)
-      // call to supabase to send message to supabase database
-      const { error } = await supabase.from("messages").insert({text});
+      setOptimisticIds(newMessage.id);
+      const { error } = await supabase.from("messages").insert({text, id});
       if(error){
         toast.error(error.message);
       }      
--- components/ListMessages.tsx
+++ components/ListMessages.tsx
@@ -1,15 +1,56 @@
 "use client";
 import { useMessage } from '@/lib/store/messages';
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Message from './Message';
 import { DeleteAlert, EditAlert } from './MessageAction';
+import { supabaseBrowser } from '@/lib/supabase/browser';
+import { toast } from 'sonner'
+import { Imessage } from '@/lib/store/messages'
 
 export default function ListMessages() {
-  const messages = useMessage((state)=>state.messages);
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const {messages, addMessage, optimisticIds, optimisticDeleteMessage, optimisticEditMessage} = useMessage((state)=>state);
+  const supabase = supabaseBrowser();
+  useEffect(() => {
+    const channel = supabase
+    .channel('chat-room')
+    .on('postgres_changes', { event: 'INSERT', schema: 'public', table: 'messages' }, 
+      async payload => {
+        if(optimisticIds.includes(payload.new.id)) return
 
+        const { error, data } = await supabase.from("users").select("*").eq("id", payload.new.send_by).single();
+        if(error){
+          toast.error(error.message);
+        }else{
+          const newMessage = {
+            ...payload.new,
+            users:data
+          } 
+          addMessage(newMessage as Imessage);
+        }        
+    })
+    .on('postgres_changes', { event: 'DELETE', schema: 'public', table: 'messages' }, payload => {
+      optimisticDeleteMessage(payload.old.id);
+    })
+    .on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'messages' }, payload => {
+      optimisticEditMessage(payload.new as Imessage);
+    })
+    .subscribe()
+
+    return () => {
+      channel.unsubscribe();
+    }
+  }, [messages]);
+
+  useEffect(() => {
+    const scrollContainer = scrollRef.current;
+    if(scrollContainer) {
+      scrollContainer.scrollTop = scrollContainer.scrollHeight;
+    }
+  }, [messages]);
 
   return (
-    <div className='flex-1 flex flex-col p-5 h-full overflow-y-auto'>
+    <div className='flex-1 flex flex-col p-5 h-full overflow-y-auto' ref={scrollRef}>
       <div className='flex-1 '></div>
       <div className='space-y-7'>
         {messages.map((value : any, index : number) => {
