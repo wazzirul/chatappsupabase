8d1c931856e5e4cb0ee36f55dcea3115c064ec87
wazzirul <mwazirulazzan@gmail.com>
2024-06-18 17:56:26 +0700

    feat : finishing touch, logout, etc

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import InitUser from '@/lib/store/InitUser'
 import ChatHeader from '@/components/ChatHeader'
 import ChatInput from '@/components/ChatInput'
 import ChatMessages from '@/components/ChatMessages'
+import ChatAbout from '@/components/ChatAbout'
 // import OpenAIComponent from '@/components/OpenAI'
 
 export default async function Page() {
@@ -19,8 +20,17 @@ export default async function Page() {
     <div className='max-w-3xl mx-auto md:py-10 h-screen'>
       <div className='h-full border rounded-md flex flex-col relative'>
         <ChatHeader user={data.session?.user} />
-        <ChatMessages />
-        <ChatInput />
+
+        {data?.session?.user ? (
+            <>
+              <ChatMessages />
+              <ChatInput />
+            </>
+          ) : (
+            <ChatAbout />
+          )
+        }
+
       </div>
     </div>
 
